6950056d520e7b7444be9f8d0d6d82c4135a1600
Alex Gronskiy <agronskiy@nvidia.com>
2025-10-22 14:04:30 -0700

    wip: chenges to run command to run inside docker
    Signed-off-by: Alex Gronskiy <agronskiy@nvidia.com>

--- run.py
+++ run.py
@@ -2,13 +2,13 @@ from nemo_skills.pipeline.cli import eval, wrap_arguments
 
 eval(
     ctx=wrap_arguments(""),
-    cluster="local",
     model="meta/llama-3.1-8b-instruct",
     server_type="openai",
     server_address="https://integrate.api.nvidia.com/v1",
     benchmarks="aime25",
-    installation_command="cd /nemo_run/code && pip install -e .",
-    output_dir="/workspace/test_2",
+    # installation_command="cd /nemo_run/code && pip install -e .",
+    installation_command="echo 'nothing'",
+    output_dir="/workspace/test_3",
     skip_hf_home_check=True,
     generation_module="some.py",
 )
