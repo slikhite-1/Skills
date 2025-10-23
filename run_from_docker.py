bb71f9c8abf9aa65aaf956d443bd4e098e9ab6f2
Alex Gronskiy <agronskiy@nvidia.com>
2025-10-22 17:41:29 -0700

    wip: adds runner from within docker
    Signed-off-by: Alex Gronskiy <agronskiy@nvidia.com>

--- run_from_docker.py
+++ run_from_docker.py
@@ -3,11 +3,11 @@ from nemo_skills.pipeline.cli import eval, wrap_arguments
 eval(
     ctx=wrap_arguments(""),
     model="meta/llama-3.1-8b-instruct",
-    cluster="local",
     server_type="openai",
     server_address="https://integrate.api.nvidia.com/v1",
     benchmarks="aime25",
-    installation_command="cd /nemo_run/code && pip install -e . && pip install 'git+https://github.com/NVIDIA-NeMo/Eval#subdirectory=packages/nemo-evaluator-launcher[all]'",
+    # installation_command="cd /nemo_run/code && pip install -e .",
+    installation_command="pip install 'git+https://github.com/NVIDIA-NeMo/Eval#subdirectory=packages/nemo-evaluator-launcher[all]'",
     output_dir="/workspace/test_3",
     skip_hf_home_check=True,
     generation_module="nemo_evaluator_runner.py",
